ff4e911c450324dcf067f956a77ed3f1654cdc11
vishal <vishalrathod2038@gmail.com>
2026-02-07 13:45:48 +0530

    testing

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
+echo hello world
+# docker pull docker pull rathod2038/sample-python-flask-app:latest
 
-docker pull docker pull rathod2038/sample-python-flask-app:latest
-
-docker run -d -p 5000:5000 --name sample-python-flask-app rathod2038/sample-python-flask-app:latest
+# docker run -d -p 5000:5000 rathod2038/sample-python-flask-app:latest
