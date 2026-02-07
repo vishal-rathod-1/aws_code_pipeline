1d744e1f0444307c98874bb35a6ba334fa0aad93
vishal <vishalrathod2038@gmail.com>
2026-02-07 13:48:57 +0530

    sample changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 set -e
-echo hello world
-# docker pull docker pull rathod2038/sample-python-flask-app:latest
 
-# docker run -d -p 5000:5000 rathod2038/sample-python-flask-app:latest
+docker pull rathod2038/sample-python-flask-app:latest
+
+docker run -d -p 5000:5000 rathod2038/sample-python-flask-app:latest
